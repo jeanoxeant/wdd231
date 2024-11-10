26e8a62eb5e1706fb06e7b9c3de049dc90093617
jeanoxeant <oxe24001@byui.edu>
2024-11-10 07:50:48 -0400

    Change1

---
+++ scripts/test.js
@@ -0,0 +1,16 @@
+const today = new Date();
+
+currentyear.innerHTML = `<span class="highlight">${today.getFullYear()}</span>`;
+
+let oLastModif = new Date(document.lastModified);
+
+lastModified.innerHTML = `<span class="highlight">${oLastModif.toLocaleDateString()} ${oLastModif.toLocaleTimeString()}</span>`;
+
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+}
