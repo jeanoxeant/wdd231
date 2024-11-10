212989f5cd76541edea62f73e153ba6f48641f7b
jeanoxeant <oxe24001@byui.edu>
2024-11-10 18:56:39 -0400

    update5

--- scripts/test.js
+++ scripts/test.js
@@ -6,7 +6,7 @@ let oLastModif = new Date(document.lastModified);
 
 lastModified.innerHTML = `<span class="highlight">${oLastModif.toLocaleDateString()} ${oLastModif.toLocaleTimeString()}</span>`;
 
-function myFunction() {
+/*function myFunction() {
     var x = document.getElementById("myTopnav");
     if (x.className === "topnav") {
       x.className += " responsive";
@@ -14,3 +14,12 @@ function myFunction() {
       x.className = "topnav";
     }
 }
+*/
+const mainnav = document.querySelector('.navigation')
+const hambutton = document.querySelector('#menu');
+
+// Add a click event listender to the hamburger button and use a callback function that toggles the list element's list of classes.
+hambutton.addEventListener('click', () => {
+	mainnav.classList.toggle('show');
+	hambutton.classList.toggle('show');
+});
